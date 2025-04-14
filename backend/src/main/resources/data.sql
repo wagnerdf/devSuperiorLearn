bb88736d38aead5ca7964b0b4e1970545f64a2b8
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-04-14 16:51:18 -0300

    Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://storage.needpix.com/rsynced_images/training-course-3250007_1280.jpg', 'https://images.icon-icons.com/3480/PNG/512/course_study_book_education_learning_icon_220053.png');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES (1.0, TIMESTAMP WITH TIME ZONE '2025-07-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2025-09-13T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES (2.0, TIMESTAMP WITH TIME ZONE '2025-08-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2025-10-13T03:00:00Z', 1);
