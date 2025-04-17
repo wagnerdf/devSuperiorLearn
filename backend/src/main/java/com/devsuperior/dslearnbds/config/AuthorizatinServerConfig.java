09e067a011726e87d15cd6ce056b115d460fa6bd
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-04-17 12:11:53 -0300

    Adding exceptions, validation and security to the project

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/config/AuthorizatinServerConfig.java
@@ -0,0 +1,76 @@
+package com.devsuperior.dslearnbds.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import com.devsuperior.dslearnbds.components.JwtTokenEnhancer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizatinServerConfig extends AuthorizationServerConfigurerAdapter{
+
+	@Value("${security.oauth2.client.client-id}")
+	private String clientId;
+	
+	@Value("${security.oauth2.client.client-secret}")
+	private String clientSecret;
+	
+	@Value("${jwt.duration}")
+	private Integer jwtDuration;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private JwtAccessTokenConverter accessTokenConverter;
+	
+	@Autowired
+	private JwtTokenStore tokenStore;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private JwtTokenEnhancer tokenEnhancer;
+	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception { // Configuração da forma que vai ser feita a autenticação
+		clients.inMemory() //Processo vai ser feito em memoria
+		.withClient(clientId) // Informar o nome da aplicação
+		.secret(passwordEncoder.encode(clientSecret)) // Senha da aplicação
+		.scopes("read", "write") // Tipos de acesso da aplicação para leitura e escrita
+		.authorizedGrantTypes("password") // Tipo de acesso oauth
+		.accessTokenValiditySeconds(jwtDuration);// Tempo de expiração do token
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception { //Quem vai atorizar e a forma do token
+		
+		TokenEnhancerChain chain = new TokenEnhancerChain();
+		chain.setTokenEnhancers(Arrays.asList(accessTokenConverter, tokenEnhancer));
+		
+		endpoints.authenticationManager(authenticationManager)
+		.tokenStore(tokenStore)
+		.accessTokenConverter(accessTokenConverter)
+		.tokenEnhancer(chain);
+		
+	}
+}
